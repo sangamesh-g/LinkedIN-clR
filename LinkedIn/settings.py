17167e5e03b3e2ca0fbe0854fcb5030ccaffdab4
sangamesh <garuna217@gmail.com>
2025-08-25 12:19:37 +0530

    error

--- LinkedIn/settings.py
+++ LinkedIn/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Media uploads (for user-uploaded files if needed)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
