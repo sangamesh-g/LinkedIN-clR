e2b16562d0e4bebc57a9570f1dafe40caee31787
sangamesh <garuna217@gmail.com>
2025-09-17 20:08:49 +0530

    pagination

--- LinkedIn/settings.py
+++ LinkedIn/settings.py
@@ -172,7 +172,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-    ),
+    ), 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2,
 }
 
 # Use custom user model for accounts app
